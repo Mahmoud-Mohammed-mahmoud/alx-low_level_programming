531e6f93c12c537b1268d234c897cd970dbd456d
sadat <mmahmoud14525@gmail.com>
2023-07-13 19:00:12 -0700

    Hello World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <unistd.h>
 
 /**
- * main - Entry point 
+ *main - Entry point 
  *
  * Description: print a qoute using write function
  *    ssize_t write(int fd, const void *buf, size_t count);
@@ -9,10 +9,8 @@
  * Return: 1 (error)
 */
 
-
 int main(void)
-{	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19/n";
+{	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(1, quo, 59);
-	return (1);
-}	
+	return (1); }	
