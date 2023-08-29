b1e95f4631405718be8b7ea58c379bbed1f57c4a
Mahmoud-Mohammed-mahmoud <mmahmoud14525@gmail.com>
2023-08-29 14:18:15 -0700

    C - More singly linked lists

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -4,10 +4,10 @@
 
 /**
  * _r - reallocates memory for an array of pointers
- * to the nodes in a linked list 
+ * to the nodes in a linked list
  * @list: the old list to append
  * @size: size of the new list (always one more than the old list)
- * @new: new node to add to the list 
+ * @new: new node to add to the list
  *
  * Return: pointer to the new list
  */
